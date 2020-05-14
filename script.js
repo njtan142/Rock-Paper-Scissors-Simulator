0d864652d9cf5cd6817ccff2b3794ed5a71cbff7
njtan142 <njtan142@github.com>
2020-05-14 02:42:53 -0700

    This is Version 1.1 which a complete one but open for your lovely/savage(-_-) suggestions or comment : ) .

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 var player1;
 var player2;
+var winsplayer1 = 0;
+var winsplayer2 = 0;
+var draws = 0;
+var rounds = 0;
+
+var f;
+   
 function player1errorcheck() {
      player1 = prompt("Enter the name of first Player");
   
@@ -32,4 +39,96 @@ function enternames(){
 }
 function namesreset(){
     document.getElementById("enternames").innerHTML = "Players' names will be shown here...";
+    
 }
+function gameSimulator(){
+    let options = ["Rock", "Paper", "Scissors"];
+    var p2fire = options[Math.floor(Math.random()*options.length)];
+    var p1fire = options[Math.floor(Math.random()*options.length)];
+    
+
+    
+        console.log(p1fire + " vs " + p2fire)
+        
+         if( p2fire == "Paper" && p1fire == "Rock" ){
+            winsplayer2 +=1;
+            }
+        else if( p2fire == "Scissors" && p1fire == "Paper" ){
+            winsplayer2 +=1;
+            }
+        else if( p1fire == "Scissors" && p2fire == "Rock" ){
+            winsplayer2 +=1;
+            }
+        else if( p2fire == "Rock" && p1fire == "Paper" ){
+            winsplayer1 +=1;
+            }
+        else if(p1fire == "Rock" && p2fire == "Scissors"){
+            winsplayer1 +=1 ;
+        }
+        else if(p1fire =="Scissors" && p2fire == "Paper"){
+            winsplayer1++;
+        }
+         else if(p1fire == "Paper" && p2fire == "Paper"){
+            draws++
+         }
+          else if(p1fire == "Scissors" && p2fire == "Scissors"){
+            draws++ }
+             else if(p1fire == "Rock" && p2fire == "Rock"){
+           draws++ }
+      // console.log(winsplayer1);
+
+  /* console.log(winsplayer1);
+    console.log(winsplayer2);    
+    console.log(draws);
+*/
+}
+function gamelooper(rounds){
+    winsplayer1 = 0;
+    winsplayer2 = 0;
+    draws = 0;
+    rounds;
+    for(i = 0; i < rounds; i++ ){
+        gameSimulator();
+    }
+    if(winsplayer1 > winsplayer2){
+         f = document.createTextNode(player1 + " is the Final Winner!!!");
+    }
+    else if(winsplayer1 < winsplayer2){
+         f = document.createTextNode(player2 + " is the Final Winner!!!");
+    }
+    else{
+         f = document.createTextNode("What a rare sight... It's a Draw!!!")
+    }
+    var r1 = document.createElement(r1);
+    var r2 = document.createElement(r2);
+    var r3 = document.createElement(r3);
+    var r4 = document.createElement(r4);
+    var p1 = document.createTextNode(player1 + " has won " + winsplayer1 + " rounds!!!")
+    var p2 = document.createTextNode(player2 + " has won " + winsplayer2 + " rounds!!!")
+    var d = document.createTextNode(draws + " Draws!!!");
+   
+    r1.setAttribute("id", "gamelooper");
+    r2.setAttribute("id", "gamelooper");
+    r3.setAttribute("id", "gamelooper");
+    r4.setAttribute("id", "gamelooper");
+    
+   
+    r1.appendChild(p1);
+    document.getElementById("p1").innerHTML = "";
+    document.getElementById("p1").appendChild(r1);
+    
+    r2.appendChild(p2);
+    document.getElementById("p2").innerHTML = "";
+    document.getElementById("p2").appendChild(r2);
+    
+    r3.appendChild(d);
+    document.getElementById("d").innerHTML = "";
+    document.getElementById("d").appendChild(r3);
+    
+    r4.appendChild(f);
+    document.getElementById("f").innerHTML = "";
+    document.getElementById("f").appendChild(r4);
+    
+
+}
+
